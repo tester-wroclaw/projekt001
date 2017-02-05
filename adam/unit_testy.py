d2ae951641b684d480c31bfa2d0a473e9fe21ef1
MARTAG <maura10@wp.pl>
2017-02-05 17:18:43 +0100

    Testy dla funkcji nr 2 (f2)

--- adam/unit_testy.py
+++ adam/unit_testy.py
@@ -2,7 +2,7 @@
 import unittest
 import wroc
 
-class testdd(unittest.TestCase):
+class testf1(unittest.TestCase):
     
     def testf1_1(self):
         w=wroc.f1(0)
@@ -23,6 +23,20 @@ class testdd(unittest.TestCase):
     def testf1_5(self):
         w=wroc.f1(2,3)
         self.assertEqual(w,7)
+        
+class testf2(unittest.TestCase):
+    
+    def testf2_1(self):
+        w=wroc.f2('ala')
+        self.assertEqual(w,'a')
+        
+    def testf2_2(self):
+        w=wroc.f2([1,2,3])
+        self.assertEqual(w,1)
+        
+    def testf2_3(self):
+        w=wroc.f2([])
+        self.assertEqual(w,'BUUUUM')
         
 if __name__== '__main__':
     unittest.main()
--- adam/wroc.py
+++ adam/wroc.py
@@ -2,3 +2,10 @@
 def f1(x, y=0):
     wynik = x*x+y
     return wynik
+
+def f2(x):
+    if len(x)==0:  
+        return 'BUUUUM'
+    else:        
+        return x[0]
+   
